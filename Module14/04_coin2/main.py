985123e32ae2b12192a38c8c549023f76e77968c
Maryia Kurachova <studentkuracheva@mail.ru>
2023-08-05 01:20:51 +0300

    founded solution of 3 task, added 1 function and cycle

--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def is_point_inside_circle(x, y, r):
+    distance_squared = x**2 + y**2
+    if distance_squared <= r**2:
+        print("Монетка где-то рядом")
+    else:
+        print("Монетки в области нет")
+
+
+x = float(input("Введите координату x: "))
+y = float(input("Введите координату y: "))
+r = float(input("Введите радиус круга: "))
+
+is_point_inside_circle(x, y, r)
+
