0a51b9b3f940a76653e2953b9790ebc393e73d72
Maryia Kurachova <studentkuracheva@mail.ru>
2023-08-14 16:48:52 +0300

    used function extend and made code better

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,13 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+main_list = [1, 5, 3]
+first_side = [1, 5, 1, 5]
+second_side = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+main_list.extend(first_side)
+num_count1 = main_list.count(5)
+main_list = [num for num in main_list if num != 5]
+
+main_list.extend(second_side)
+num_count2 = main_list.count(3)
+
+print("итоговый список:", main_list)
+print("кол-во пятёрок:", num_count1, "кол-во троек:", num_count2)
