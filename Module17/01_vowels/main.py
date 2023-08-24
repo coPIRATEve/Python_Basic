c1b1d43d67a44da2523d0c10011681b884f5065c
Maryia Kurachova <studentkuracheva@mail.ru>
2023-08-24 18:29:00 +0300

    finished homework 17

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def finder(text):
+    vowels = 'аеёиоуыэюя'
+    vowel_list = [char for char in text.lower() if char in vowels]
+    return vowel_list
+
+user_input = input("Введите текст на русском языке: ")
+vowels_list = finder(user_input)
+
+print("Список гласных букв:", vowels_list)
+print("Длина списка:", len(vowels_list))
+
--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+alphabet = ['АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' , 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя']
+letter = input('Введите сообщение: ')
+k = int(input('Введите сдвиг: '))
+cezar_letter = ''
+for ch in letter:
+  for r in alphabet:
+    if (p := r.find(ch)) >= 0:
+      ch = r[(p + k) % len(r)]
+      break
+  cezar_letter += ch
+print('Зашифрованное сообщение:', cezar_letter)
