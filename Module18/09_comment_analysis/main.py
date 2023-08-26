3639f06176801093c190e6cb06516977cfa23b78
Maryia Kurachova <studentkuracheva@mail.ru>
2023-08-26 19:33:39 +0300

    finished homework 18

--- Module18/09_comment_analysis/main.py
+++ Module18/09_comment_analysis/main.py
@@ -3,13 +3,13 @@ def count_uppercase_lowercase(text):
     uppercase_count = 0
     lowercase_count = 0
 
-    for char in text:
-        if char.isupper():
+    for _ in text:
+        if _.isupper():
             uppercase_count += 1
-        elif char.islower():
+        elif _.islower():
             lowercase_count += 1
 
-    return uppercase_count, lowercase_count
+    return uppercase_count, lowercase_counе
 
 
 text = input("Введите строку для анализа: ")
