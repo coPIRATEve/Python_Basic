cf80706b705716ee4130349a9fbbb2bf58e30377
Maryia Kurachova <studentkuracheva@mail.ru>
2023-10-02 00:34:21 +0300

    finished homework 21

--- Module21/03_deep_copy/main.py
+++ Module21/03_deep_copy/main.py
@@ -1,14 +1,31 @@
-site = {
-    'html': {
-        'head': {
-            'title': 'Куплю/продам телефон недорого'
-        },
-        'body': {
-            'h2': 'У нас самая низкая цена на iphone',
-            'div': 'Купить',
-            'p': 'продать'
+import copy
+def find_key(struct,key,name):
+    if key in struct:
+        struct[key] = name
+    for substruct in struct.values():
+        if isinstance(substruct,dict):
+            result = find_key(substruct, key, name)
+            if result:
+                return struct
+
+clients = []
+sites = int(input("Кол-во сайтов: "))
+for _ in range(sites):
+    product = input("Введите название для нового сайта: ")
+    site = {
+        'html': {
+            'head': {
+                'title': f'Куплю/продам {product} недорого'
+            },
+            'body': {
+                'h2': f'У нас самая низкая цена на {product}',
+                'div': 'Купить',
+                'p': 'Продать'
+            }
         }
     }
-}
-
-# TODO здесь писать код
+    for info in site:
+        find_key(site ,info ,site[info])
+    clients.append((f'\nСайт для {product}:', f'{copy.deepcopy(site)}'))
+    for client in clients:
+        print('\n'.join(client))
