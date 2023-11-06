785c0c5290c6fef8776491bc10ec6161cbb4786a
Maryia Kurachova <studentkuracheva@mail.ru>
2023-11-06 20:46:55 +0300

    finished homework 25

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+import os
+def gen_files_path(directory='/', target_directory=None):
+    file_paths = []
+
+    for root, _, files in os.walk(directory):
+        if target_directory is None or os.path.abspath(root) == os.path.abspath(target_directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+
+    return file_paths
+
+target_directory = 'C:\\'
+file_paths = gen_files_path(target_directory=target_directory)
+
+for file_path in file_paths:
+    print(file_path)
--- Module26/05_logs_processing/main.py
+++ Module26/05_logs_processing/main.py
@@ -1,16 +1,18 @@
-# TODO здесь писать код
+import os
+def error_log_generator(file_path):
+    with open(file_path) as file:
+        for line in file:
+            if 'ERROR' in line:
+                yield line
 
 
-# При помощи модуля os (и функции join) сформируйте пути до файлов work_logs.txt и output.txt в папке data
-# (output.txt может не быть в папке data, но его нужно будет там создать, при помощи кода)
-input_file_path = ...
-output_file_path = ...
-# Документация по join https://docs-python.ru/standart-library/modul-os-path-python/funktsija-join-modulja-os-path/
+def main():
+    input_file = 'input.txt'
+    output_file = 'output.txt'
+    with open(output_file, 'w') as out_file:
+        for line in error_log_generator(input_file):
+            out_file.write(line)
 
-# Не забудьте проверить наличие файлов перед тем как начать работу с ними
-# https://docs-python.ru/standart-library/modul-os-path-python/funktsija-exists-modulja-os-path/
 
-with open(output_file_path, 'w') as output:
-    for error_line in error_log_generator(input_file_path):
-        output.write(error_line)
-print("Файл успешно обработан.")
+if __name__ == '__main__':
+    main()
