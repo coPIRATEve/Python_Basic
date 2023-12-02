92e331451939a250e3bfc0edb2bffc7e3266e06c
Maryia Kurachova <studentkuracheva@mail.ru>
2023-12-02 17:48:32 +0300

    finished homework 29

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+class PermissionError(Exception):
+    pass
+
+def check_permission(required_permission):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            user_permissions = ['admin']
+
+            if required_permission in user_permissions:
+                return func(*args, **kwargs)
+            else:
+                raise PermissionError(f"У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}")
+
+        return wrapper
+    return decorator
+
+
+user_permissions = ['admin']
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+delete_site()
+try:
+    add_comment()
+except PermissionError as e:
+    print(e)
+
--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def singleton(cls):
+    instances = {}
+
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+@singleton
+class Numbers:
+    def __init__(self, value):
+        self.value = value
+
+obj1 = Numbers(42)
+print(obj1.value)
+
+obj2 = Numbers(99)
+print(obj2.value)
+
+print(obj1 is obj2)
+
