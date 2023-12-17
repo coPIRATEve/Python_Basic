0d882945c39b77f4f5d36a594c8a189332d5c46d
Maryia Kurachova <studentkuracheva@mail.ru>
2023-12-17 20:46:29 +0300

    finished homework 30

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -6,9 +6,9 @@ numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
 
 powered_floats = list(map(lambda x: round(x ** 3, 3), floats))
 filtered_names = list(filter(lambda x: len(x) >= 5, names))
-product_of_numbers = lambda x: x if not x else x[0] * product_of_numbers(x[1:])
+product_of_numbers = lambda x: 1 if not x else x[0] * product_of_numbers(x[1:])
 result_numbers = product_of_numbers(numbers)
 
-print("Powered Floats:", powered_floats)
-print("Filtered Names:", filtered_names)
-print("Product of Numbers:", result_numbers)
+print(" из списка floats возводится в третью степень и округляется до трёх знаков после запятой:", powered_floats)
+print("Из списка names берутся только те имена, в которых есть минимум пять бук:", filtered_names)
+print("Из списка numbers берётся произведение всех чисел:", result_numbers)
