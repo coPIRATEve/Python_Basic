6e15d56a05d9ffd4365604c81a4fdcb907a0ac1d
Alexey <alexei@bk.ru>
2023-12-18 15:40:03 +0300

    fix

--- Module30/01_new_lists/main.py
+++ Module30/01_new_lists/main.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import List
 
 floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
@@ -6,8 +7,7 @@ numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
 
 powered_floats = list(map(lambda x: round(x ** 3, 3), floats))
 filtered_names = list(filter(lambda x: len(x) >= 5, names))
-product_of_numbers = lambda x: 1 if not x else x[0] * product_of_numbers(x[1:])
-result_numbers = product_of_numbers(numbers)
+result_numbers = reduce(lambda num1, num2: num1 * num2, numbers)
 
 print(" из списка floats возводится в третью степень и округляется до трёх знаков после запятой:", powered_floats)
 print("Из списка names берутся только те имена, в которых есть минимум пять бук:", filtered_names)
