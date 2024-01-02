f9e360d265ec52943923a4ce6d273ca0c2d81a95
Maryia Kurachova <studentkuracheva@mail.ru>
2024-01-02 17:27:44 +0300

    finished homework 31

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -1,20 +1,29 @@
 import json
 
-def load_json(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+def compare_json(json_old, json_new, diff_list):
+    result = {}
 
-data_old = load_json('json_old.json')
-data_new = load_json('json_new.json')
+    for key in json_old:
+        if key in diff_list and json_old[key] != json_new[key]:
+            result[key] = {
+                "old_value": json_old[key],
+                "new_value": json_new[key]
+            }
+            print(f"Difference in {key}: {json_old[key]} -> {json_new[key]}")
+
+    return result
 
 diff_list = ["services", "staff", "datetime"]
-result = {}
 
-for key in diff_list:
-    if key in data_old and key in data_new and data_old[key] != data_new[key]:
-        result[key] = data_new[key]
+with open('json_old.json', 'r') as f_old:
+    data_old = json.load(f_old)
+
+with open('json_new.json', 'r') as f_new:
+    data_new = json.load(f_new)
+
+result = compare_json(data_old, data_new, diff_list)
 
 print(result)
 
-with open('result.json', 'w', encoding='utf-8') as result_file:
-    json.dump(result, result_file, ensure_ascii=False, indent=2)
+with open('result.json', 'w') as f_result:
+    json.dump(result, f_result, ensure_ascii=False, indent=2)
